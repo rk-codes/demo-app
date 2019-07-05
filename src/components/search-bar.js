c9ae225918c748304e4f93cbaac98247ca555990
rk-codes <ping.you.back@gmail.com>
2019-07-04 19:27:39 -0700

    add function

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 import '../css/search-bar.css';
 
 export default class  SearchBar extends React.Component {
+
+    sayHellooo() {
+        return (
+            <div>Helllooooooo</div>
+        )
+    }
    
     render() {
         return(
             <div className="search-box">
                 <form className="search-form" >
                     <input type="text" name="location" placeholder="Enter city or state" />
+                    <input type="text" name="test" placeholder="Enter something" />
                     <button  className="search-button">Go</button>
                 </form>
             </div>
